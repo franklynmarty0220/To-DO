1b27e38640edfaa4cc888971451e5e1156a1d1f4
franklynmarty0220 <64612023+franklynmarty0220@users.noreply.github.com>
2021-09-22 12:52:10 -0400

    Update app.js

--- app.js
+++ app.js
@@ -6,16 +6,18 @@ const list = document.querySelector("#todo-list");
 
 
 
-
 const savedTodos = JSON.parse(localStorage.getItem("todos")) || [];
+
 for (let i = 0; i < savedTodos.length; i++) {
-  let newTodo = document.createElement("li");
-  newTodo.innerText = savedTodos[i].task;
-  newTodo.isComupleted = savedTodos[i].isCompleted ? true : false;
-  
+  //let newTodo = document.createElement("li");
+  //newTodo.innerText = savedTodos[i].task;
+  const newTodo = createTodoListItem(savedTodos[i].task);
+  newTodo.isCompleted = savedTodos[i].isCompleted ? true : false;
+   
   
   list.appendChild(newTodo);
-}
+};
+
 
 list.addEventListener('click', function(e){
     const elm = e.target.parentNode;
@@ -32,25 +34,41 @@ list.addEventListener('click', function(e){
 
 });
 
+function createTodoListItem(text){
+    const newToDo = document.createElement("li");
+    const removeBtn = document.createElement("button");
+    newToDo.innerText = text;
+    removeBtn.value = "Complete!";
+
+    newToDo.appendChild(removeBtn);
+    return newToDo;
+    
+    
+}
+
 
 
 form.addEventListener('submit',function(e){
     e.preventDefault();
 
-    const newToDo = document.createElement('li');
-    const removeBtn = document.createElement("button");
+    //const newToDo = document.createElement('li');
+    //const removeBtn = document.createElement("button");
     
-    newToDo.innerText = input.value;
-    input.value = ''
-    removeBtn.innerText = 'Complete!'
+   // newToDo.innerText = input.value;
+    //input.value = ''
+    //removeBtn.innerText = 'Complete!'
     
-    newToDo.appendChild(removeBtn);
-    list.appendChild(newToDo);
-    form.reset();
+    
+    //list.appendChild(newToDo);
+    //form.reset();
 
+    const newToDo = createTodoListItem(input.value);
+    list.appendChild(newToDo);
     savedTodos.push({ task: newToDo.innerText, isCompleted: false});
     localStorage.setItem("todos", JSON.stringify(savedTodos));
 
 });
+
+
 
 
